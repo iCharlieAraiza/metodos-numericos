e3144901cd145300ab86546d30872e787d26a0f6
iCharlieAraiza <eliptikman1234@gmail.com>
2020-09-28 12:41:32 -0500

    Update biseccion.cpp

--- biseccion.cpp
+++ biseccion.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 static double funcion(double x);
 int main() {
-  double xi = -1;
-  double xf = 3;
+  double xi = 0.1;
+  double xf = 7;
   double xr = 1;
 
   if (funcion(xi) * funcion(xf) > 0.0f)
@@ -12,7 +12,7 @@ int main() {
     cout << "la función tiene el mismo signo";
     return -1;
   }
-  while ( abs(xi - xf) > 0.00001)
+  while ( abs(xi - xf) > 0.001)
   {
     xr = (xi + xf) / 2.0f;
     cout << "X: " << xr << endl;
@@ -32,5 +32,6 @@ int main() {
 
 static double funcion(double x)
 { 
-  return 1-((20*20)/(9.98)*(3*x+(x*x)/2))*(3+x);
+  double el =(0.5*x*x+3*x)*(0.5*x*x+3*x)*(0.5*x*x+3*x);
+  return 1-(40.77471*(x+3))/el;
 }
